a664d63406d70fdb739e328fb49112c2cfe29109
MargaritaSukhova <MargaritaSukhova@users.noreply.github.com>
2023-09-17 17:35:39 +0000

    deploy: 67b04be094b2d01a58b330ae59c5b80a80391441

---
+++ assets/HomePage-7bb4478a.js
@@ -0,0 +1,39 @@
+import{n,j as e,H as r,N as l,B as o}from"./index-9f1fa782.js";const c="/cars-ua/assets/calendar-d8f885b4.png",x="/cars-ua/assets/magnifier-ab74691c.png",h="/cars-ua/assets/wallet-8afb17af.png",d="/cars-ua/assets/clean-444ae1c8.png",f=n.ul`
+	margin-top: 50px;
+  margin-left: 400px;
+  margin-bottom: 50px;
+	display: flex;
+	flex-direction: column;
+	justify-items: flex-end;
+  gap: 20px;
+`,t=n.li`
+	display: flex;
+	gap: 14px;
+`,s=n.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: center;
+`,a=n.h2`
+	color: var(--White, #fff);
+	font-family: Manrope;
+	font-size: 24px;
+	font-style: normal;
+	font-weight: 600;
+	line-height: 28px;
+`,i=n.p`
+	color: rgba(18, 20, 23, 0.5);
+	font-family: Manrope;
+	font-size: 18px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: 24px;
+`,p=()=>e.jsxs(f,{children:[e.jsxs(t,{children:[e.jsx("img",{src:c,alt:"Calendar",width:"70"}),e.jsxs(s,{children:[e.jsx(a,{children:"Flexible rentals"}),e.jsx(i,{children:"Cancel or change most bookings for free up to 48 hours before pick-up"})]})]}),e.jsxs(t,{children:[e.jsx("img",{src:x,alt:"Magnifier",width:"70"}),e.jsxs(s,{children:[e.jsx(a,{children:"No hidden fees"}),e.jsx(i,{children:"Know exactly what you’re paying"})]})]}),e.jsxs(t,{children:[e.jsx("img",{src:h,alt:"Wallet",width:"70"}),e.jsxs(s,{children:[e.jsx(a,{children:"Price Match Guarantee"}),e.jsx(i,{children:"Found the same deal for less? We’ll match the price"})]})]}),e.jsxs(t,{children:[e.jsx("img",{src:d,alt:"Clean",width:"70"}),e.jsxs(s,{children:[e.jsx(a,{children:"Clean cars. Socially distant rental counters."}),e.jsx(i,{children:"We’re working with our partners to keep you safe and in the driving seat"})]})]})]}),g=n.h1`
+	color:  #fff;
+  text-align: center;
+	font-family: Manrope;
+	font-size: 36px;
+	font-style: normal;
+	font-weight: 600;
+	line-height: 48px;
+`,j=()=>e.jsxs(r,{children:[e.jsx(g,{children:"Cars UA – Search, Compare & Save"}),e.jsx(p,{}),e.jsx(l,{to:"/catalog",children:e.jsx(o,{type:"button",children:"Book now"})})]});j.propTypes={};export{j as default};
